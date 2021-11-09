27d882d7a915c4de009b641aba1e86158e7f7f8b
Craig <craig.coleman.cc@gmail.com>
2021-11-09 13:08:09 -0600

    commit tree 4296ef8443cf33fe34648d10819ccd8c1e30ffe5

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData (userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        document.querySelector('body').append(object.id);
+    })
+    .catch(function (error) {
+        document.querySelector('body').append(error.message);
+    })
+}
+
+
